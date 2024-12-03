8b9593afb9d038190f478c749c3174fcab8f25fa
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2024-12-03 20:20:40 +0100

    feat: add, subtract, multiply, divide functions

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let num1;
 let operator;
 let num2;
 
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+    return numOne / numTwo;
+}
